13efb2b0947446e22871b25242e1670ceab5c7e7
rodguit <142036086+rodguit@users.noreply.github.com>
2023-10-20 22:24:26 -0300

    operadores de funcoes

--- Expressoes-funcao.js
+++ Expressoes-funcao.js
@@ -7,7 +7,40 @@ minhaFuncao("param")
 
 //expressão de função
 
-const soma = function(n1, n2){
-    return n1 + n2
+//const soma = function(n1, n2){
+ //   return n1 + n2
+//}
+//console.log(soma(1, 1))
+
+
+// difernça pricipal: hoisting
+// 
+
+//function apresentar() {
+ //   return "ola";
+//}
+
+//console.log(soma(1, 1))
+//const soma = function(n1, n2){
+//    return n1 + n2;
+//}
+
+// Arrow function
+function apresentar(nome){
+    return `Meu nome é ${nome}`;
 }
-console.log(soma(1, 1))
+const apresentarArrow = nome => `Meu nome é ${nome}`;
+const soma = (n1, n2) => n1 + n2;
+
+//Arrow function com + de 1 linha de instrução
+
+const somaNumerosPequenos = (n1, n2) => {
+    if (n1 > 10 || n2 > 10){
+        return "Somente numeros de 1 a 9"
+    } else {
+        return n1 + n2;
+    }
+}
+
+
+
